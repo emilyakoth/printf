683dd6d79f0123065ce2e2e2663ac65a93e21c3f
emilyakoth <emilyakoth9@gmail.com>
2023-04-24 01:23:14 -0700

    printf project

---
+++ _printf.c
@@ -0,0 +1,62 @@
+#include "main.h"
+void print_buffer(char buffer[], int *buff_ind);
+
+/**
+ * _printf- prints function
+ * @format: formt
+ * Return: function
+ */
+int _printf(const char *format, ...)
+{
+	int i, printed = 0, printed_chars = 0;
+	int flag, width, size, precision, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind)
+			/* write(1, &format[i], 1);*/
+			printed chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			i++;
+			printed = handle_print(format, &i, list, buffer,
+					flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+				printed_chars += printed;
+		}
+	}
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+	return (printed_chars);
+}
+/**
+ * print_buffer - prints the buffer and its contents
+ * @buffer: array
+ * @buff_ind: lenghth index
+ * Return: nothing
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
