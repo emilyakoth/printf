5f14ce9b4540ac62c26590715a9ba118c52f2bcc
R0dR1ck-M <rodrickmutai@gmail.com>
2023-04-25 04:09:03 -0700

    Betty Convert Numbers

--- convert_numbers.c
+++ convert_numbers.c
@@ -2,13 +2,13 @@
 include "main.h"
 
 unsigned int convert_di(va_list args, buffer_t *output, unsigned char flags,
-	       	int wid, int prec, unsigned char len);
+	int wid, int prec, unsigned char len);
 unsigned int convert_b(va_list args, buffer_t *output, unsigned char flags,
-	       	int wid, int prec, unsigned char len);
+	int wid, int prec, unsigned char len);
 unsigned int convert_u(va_list args, buffer_t *output, unsigned char flags,
-	       	int wid, int prec, unsigned char len);
+	int wid, int prec, unsigned char len);
 unsigned int convert_o(va_list args, buffer_t *output, unsigned char flags,
-	       	int wid, int prec, unsigned char len);
+	int wid, int prec, unsigned char len);
 
 /**
  * convert_di - Converts an argument to a signed int and stores
@@ -24,7 +24,7 @@ unsigned int convert_o(va_list args, buffer_t *output, unsigned char flags,
  */
 
 unsigned int convert_di(va_list args, buffer_t *output, unsigned char flags,
-	       	int wid, int prec, unsigned char len)
+	int wid, int prec, unsigned char len)
 {
 	long int d copy;
 	unsigned int ret = 0, count = 0;
@@ -72,7 +72,6 @@ unsigned int convert_di(va_list args, buffer_t *output, unsigned char flags,
 	/*Handle plus flag when zero flag is not active*/
 	if (ZERO_FLAG == 0 && (PLUS_FLAG == 1 && d >= 0))
 		ret += _memcpy(output, &plus, 1);
-
 	if (!(d == 0 && prec == 0))
 		ret += convert_sbase(output, d, "0123456789", flags, 0, prec);
 	ret += print_neg_width(output, ret, flags, wid);
@@ -93,7 +92,7 @@ unsigned int convert_di(va_list args, buffer_t *output, unsigned char flags,
  */
 
 unsigned int convert_b(va_list args, buffer_t *output, unsigned char flags,
-	       	int wid, int prec, unsigned char len)
+	int wid, int prec, unsigned char len)
 {
 	unsigned int num;
 
@@ -118,7 +117,7 @@ unsigned int convert_b(va_list args, buffer_t *output, unsigned char flags,
  */
 
 unsigned int convert_o(va_list args, buffer_t *output, unsigned char flags,
-	       	int wid, int prec, unsigned char len)
+	int wid, int prec, unsigned char len)
 {
 	unsigned long int num;
 	unsigned int ret = 0;
@@ -156,7 +155,7 @@ unsigned int convert_o(va_list args, buffer_t *output, unsigned char flags,
  */
 
 unsigned int convert_u(va_list args, buffer_t *output, unsigned char flags,
-	       	int wid, int prec, unsigned char len)
+	int wid, int prec, unsigned char len)
 {
 	unsigned long int num;
 	unsigned int ret = 0;
